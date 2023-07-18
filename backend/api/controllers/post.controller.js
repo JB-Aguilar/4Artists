5b6cca15f9fe37578dee0439553ad4af052788bf
Juan <jbarrosoaguilar@gmail.com>
2023-07-18 16:45:45 +0100

    Add likes and images in posts

--- backend/api/controllers/post.controller.js
+++ backend/api/controllers/post.controller.js
@@ -1,13 +1,19 @@
 const { Posts } = require ('../models/post.model')
 const { User } = require("../models/user.model.js");
 const { Like } = require("../models/like.model.js");
+const cloudinary = require('../../db/cloudinary');
 
 const getPosts = async(req, res) => {
     try {
         const post = await Posts.findAll({
-          include: User
+          include: User,
         });
-        res.status(200).json( post )
+        const likes = await Like.findAll()
+        const data = {
+            post: post,
+            likes: likes
+        }
+        res.status(200).json( data )
     }catch(error){
         res.status(500).send('Error: posts not found')
     }
@@ -28,6 +34,11 @@ const getOnePost = async(req, res) => {
 const createPost = async(req, res) => {
     try{
         if(!req.body.userId) {req.body.userId = res.locals.user.id}
+
+        if (req.file) {
+          const result = await cloudinary.uploader.upload(req.file.path);
+          req.body.img = result.secure_url;
+        }
 
         const post = await Posts.create(req.body)
         return res.status(200).json({ message: 'Success', post: post})
--- backend/db/relationships.js
+++ backend/db/relationships.js
@@ -1,13 +1,14 @@
 const { User } = require('../api/models/user.model')
 const { Posts } = require("../api/models/post.model");
+const { Like } = require('../api/models/like.model');
 
 const createRelations = async () => {
     
     User.hasMany(Posts, { onUpdate: "CASCADE", onDelete: "CASCADE" });
     Posts.belongsTo(User);
 
-    User.belongsToMany(Posts, {through: 'like', timestamps: false})
-    Posts.belongsToMany(User, {through: 'like', timestamps: false})
+    User.belongsToMany(Posts, {through: Like, timestamps: false})
+    Posts.belongsToMany(User, {through: Like, timestamps: false})
 
 }
 
